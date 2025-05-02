611e91c8cc8e4317ce636ab1507e61af5a23bab9
adriesco <144839808+adriesco@users.noreply.github.com>
2025-05-02 17:18:37 +0200

    Cambios en los colores

--- mi-trabajo/src/components/Footer.jsx
+++ mi-trabajo/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="bg-green-600 text-white py-4 mt-8">
+    <footer className="bg-black text-white py-6 px-4 shadow-md">
       <div className="container mx-auto text-center">
         <p>&Copy; 2025 GoFootball. Todos los derechos reservados.</p>
         <div className="mt-4"></div>
--- mi-trabajo/src/components/Home.jsx
+++ mi-trabajo/src/components/Home.jsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 
 function Home() {
   return (
-    <main className="flex flex-col items-center justify-center flex-grow text-center p-6">
+    <main className="flex flex-col items-center justify-center flex-grow text-center p-6 bg-gradient-to-tr from-black to-green-500 text-white">
       <h1 className="text-4xl font-extrabold mb-4 opacity-0 animate-fade-in delay-100">
-        Bienvenido a <span className="text-yellow-300">GoFootball</span>
+        Bienvenido a <span className="text-green-400">GoFootball</span>
       </h1>
 
       <p className="text-lg mb-6 opacity-0 animate-fade-in delay-300">
@@ -15,13 +15,13 @@ function Home() {
       <div className="flex gap-5 opacity-0 animate-fade-in delay-500">
         <Link
           to="/registro"
-          className="px-6 py-3 bg-yellow-400 text-green-900 font-semibold rounded-lg hover:bg-yellow-500 transition duration-300 transform hover:scale-105"
+          className="px-6 py-3 bg-green-500 text-green-950 font-semibold rounded-lg hover:bg-green-400 transition duration-300 transform hover:scale-105"
         >
           Registrarse
         </Link>
         <Link
           to="/iniciarsesion"
-          className="px-6 py-3 bg-blue-400 text-white font-semibold rounded-lg hover:bg-blue-500 transition duration-300 transform hover:scale-105"
+          className="px-6 py-3 bg-green-500 text-green-950 font-semibold rounded-lg hover:bg-green-400 transition duration-300 transform hover:scale-105"
         >
           Iniciar Sesión
         </Link>
